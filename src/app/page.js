2e0c5dcc8a71a103642b9b66defc20f2a90dafb5
Moamal-2000 <moamalalaapro1@gmail.com>
2025-12-29 14:03:49 +0300

    feat(metadata): add getOpenGraphMetadata() to generate openGraph object

--- src/app/page.js
+++ src/app/page.js
@@ -1,4 +1,9 @@
+import { getOpenGraphMetadata } from "@/data/metadata";
 import s from "./page.module.scss";
+
+export function generateMetadata() {
+  return getOpenGraphMetadata();
+}
 
 export default function Home() {
   return (
--- src/data/metadata.js
+++ src/data/metadata.js
@@ -5,4 +5,29 @@ const description = "";
 
 const GLOBAL_METADATA = { title, description, keywords };
 
+export function getOpenGraphMetadata(pagePath = "") {
+  const url = pagePath ? `${BASE_URL}/${pagePath}` : BASE_URL;
+
+  return {
+    openGraph: {
+      title,
+      description,
+      url,
+      type: "website",
+      locale: "en_US",
+      siteName: "",
+      authors: ["Moamal Alaa"],
+      images: [
+        {
+          url: `${BASE_URL}/og-image.webp`,
+          type: "image/webp",
+          alt: "",
+          width: 1200,
+          height: 634,
+        },
+      ],
+    },
+  };
+}
+
 export const METADATA = { ...GLOBAL_METADATA };
