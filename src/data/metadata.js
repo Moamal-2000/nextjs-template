373496786435d67732a0116f70ce9a7a60c4e7cf
Moamal-2000 <moamalalaapro1@gmail.com>
2025-12-29 14:02:44 +0300

    feat(metadata): prepare main metadata object

--- src/data/metadata.js
+++ src/data/metadata.js
@@ -1,1 +1,8 @@
-export const BASE_URL = "http://localhost:3000";
+export const BASE_URL = "https://localhost:3000";
+const title = "";
+const keywords = [];
+const description = "";
+
+const GLOBAL_METADATA = { title, description, keywords };
+
+export const METADATA = { ...GLOBAL_METADATA };
